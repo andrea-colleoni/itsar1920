a80cb31e1d52e58f3f3eb9160f50b921f4e37248
Andrea Colleoni <andrea@colleoni.info>
2019-12-05 15:01:01 +0100

    networking (2)

--- modulo-3/doc-20191205/src/networking/ProgrammaServer.java
+++ modulo-3/doc-20191205/src/networking/ProgrammaServer.java
@@ -1,21 +1,28 @@
 package networking;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProgrammaServer {
+
+	private static List<PrintWriter> remoteStreams = new ArrayList<>();
 
 	public static void main(String[] args) throws IOException {
 		ServerSocket ss = new ServerSocket(5000);
 		System.out.println("il server ascolta sulla porta 5000");
-		while(true) {
+		while (true) {
 			Socket s = ss.accept();
+			remoteStreams.add(new PrintWriter(s.getOutputStream()));
 			new Thread(() -> {
 				try {
-					chat(("Client"), s);
+					chat(s);
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -23,13 +30,36 @@ public class ProgrammaServer {
 		}
 	}
 
-	public static void chat(String nomeClient, Socket s) throws IOException {
-		System.out.println(nomeClient + " connesso sulla porta " + s.getRemoteSocketAddress());
+	public static void chat(Socket s) throws IOException {
+		System.out.println(s.getRemoteSocketAddress() + " connesso ");
 		// ho il socket.... posso leggere
 		BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+		PrintWriter pw = new PrintWriter(s.getOutputStream());
+		pw.println("Benvenuto nella chat. Digita \"exit\" per uscire.");
+		pw.print("Insierisci il tuo nome: ");
+		pw.flush();
+		String nomeClient = br.readLine();
+		System.out.println(nomeClient + " è entrato nella chat");
 		String dati = br.readLine();
 		while (s.isConnected() && dati != null && !"exit".equals(dati)) {
-			System.out.println(nomeClient + ": " + dati);
+
+			if ("get".contentEquals(dati)) {
+				String file = br.readLine();
+				BufferedReader fbr = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+				String str = fbr.readLine();
+				while (str != null) {
+					pw.println(str);
+					str = fbr.readLine();
+				}
+				pw.flush();
+				fbr.close();
+			} else {
+				System.out.println(nomeClient + ": " + dati);
+				for (PrintWriter p : remoteStreams) {
+					p.println(nomeClient + ": " + dati);
+					p.flush();
+				}
+			}
 			dati = br.readLine();
 		}
 		System.out.println(nomeClient + " se n'è andato!");
